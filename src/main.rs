d6bc4546bfe201caa32ab99c9228c49bfc512784
Naoki Sawada <sawachon.21@gmail.com>
2020-10-23 14:18:31 +0900

    Fix auth
    Authentication applies only to the "/echo" path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod auth;
 
 use actix_cors::Cors;
-use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{get, middleware, web, App, Error, HttpResponse, HttpServer};
 use actix_web_httpauth::middleware::HttpAuthentication;
 use env_logger;
 use log::info;
@@ -17,16 +17,15 @@ async fn hello() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().body("Hello world!"))
 }
 
-#[post("/echo")]
+#[get("/hey")]
+async fn manual_hello() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().body("Hey there!"))
+}
+
 async fn echo(req: web::Json<EchoRequest>) -> Result<HttpResponse, Error> {
     let res: String = format!("Hello, {}.", req.name);
     info!("{}", res);
     Ok(HttpResponse::Ok().body(res))
-}
-
-#[get("/hey")]
-async fn manual_hello() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().body("Hey there!"))
 }
 
 #[actix_web::main]
@@ -38,10 +37,13 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default().send_wildcard();
         App::new()
             .wrap(middleware::Logger::default())
-            .wrap(HttpAuthentication::bearer(auth::validator))
             .wrap(cors)
+            .service(
+                web::scope("/echo")
+                    .wrap(HttpAuthentication::bearer(auth::validator))
+                    .route("", web::post().to(echo)),
+            )
             .service(hello)
-            .service(echo)
             .service(manual_hello)
     })
     .bind("127.0.0.1:8080")?
